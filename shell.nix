f9a76ef5f9932375590e45003d3e3debcc262814
Rakshan Shetty <shetty.raxx555@gmail.com>
2023-04-21 15:38:53 +0530

    update

--- shell.nix
+++ shell.nix
@@ -12,27 +12,28 @@ let
     cmake
     gcc12
     clang_13
+    faiss
   ];
 
   inputs = basePackages ++ lib.optionals stdenv.isLinux [ inotify-tools ]
     ++ lib.optionals stdenv.isDarwin
-    (with darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]);
+    (with darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices Security SystemConfiguration Foundation ]);
 
   hooks = ''
-     mkdir -p .nix-mix .nix-hex .livebook
-     export MIX_HOME=$PWD/.nix-mix
-     export HEX_HOME=$PWD/.nix-hex
+    mkdir -p .nix-mix .nix-hex .livebook
+    export MIX_HOME=$PWD/.nix-mix
+    export HEX_HOME=$PWD/.nix-hex
 
-     export MIX_PATH="${beam.packages.erlangR25.hex}/lib/erlang/lib/hex/ebin"
-     export PATH=${erlangR25}/bin:$MIX_HOME/bin:$HEX_HOME/bin:$MIX_HOME/escripts:bin:$PATH
+    export MIX_PATH="${beam.packages.erlangR25.hex}/lib/erlang/lib/hex/ebin"
+    export PATH=${erlangR25}/bin:$MIX_HOME/bin:$HEX_HOME/bin:$MIX_HOME/escripts:bin:$PATH
 
-    ls "${pkgs.llvm_13}/bin"
+    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
 
-     export LANG=C.UTF-8
-     # keep your shell history in iex
-     export ERL_AFLAGS="-kernel shell_history enabled"
+    export LANG=C.UTF-8
+    # keep your shell history in iex
+    export ERL_AFLAGS="-kernel shell_history enabled"
 
-     export MIX_ENV=dev
+    export MIX_ENV=dev
   '';
 in
 
